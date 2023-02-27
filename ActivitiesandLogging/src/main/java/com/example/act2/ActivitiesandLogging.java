fe3df909504cc3f0b6a0b2e1a61f3061952eaea4
rowelpadillo <rowelpadillo@gmail.com>
2023-02-27 19:10:43 +0800

    Elec 2 Activities

---
+++ ActivitiesandLogging/src/main/java/com/example/act2/ActivitiesandLogging.java
@@ -0,0 +1,38 @@
+package com.example.act2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivitiesandLogging extends AppCompatActivity {
+
+    private static final String TAG = "ActivitiesandLogging";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activitiesand_logging);
+
+        Log.v(TAG,  "This is a verbose log.");
+        Log.d(TAG,  "This is a debug log.");
+        Log.i(TAG,  "This is a info log.");
+        Log.w(TAG,  "This is a warn log.");
+        Log.e(TAG,  "This is a error log.");
+
+        Button button = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+                Intent intent = new Intent(ActivitiesandLogging.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ act3/src/main/java/com/example/act3/ResourcesandLayouts.java
@@ -0,0 +1,14 @@
+package com.example.act3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResourcesandLayouts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resourcesand_layouts);
+    }
+}
